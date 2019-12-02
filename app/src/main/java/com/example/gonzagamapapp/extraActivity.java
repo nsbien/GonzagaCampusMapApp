b1fe0df6433161d104d50c5893c7e3df3951e048
Joseph Torii <jtorii@zagmail.gonzaga.edu>
2019-12-01 20:14:38 -0800

    trying to figure out web scraping

--- app/src/main/java/com/example/gonzagamapapp/extraActivity.java
+++ app/src/main/java/com/example/gonzagamapapp/extraActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.jsoup.Jsoup;
@@ -22,22 +24,44 @@ public class extraActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
 
+        texx = (TextView)findViewById(R.id.tex1);
+
+        Button but = (Button) findViewById(R.id.but1);
+        but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                new doIt().execute();
+
+            }
+        });
+
     }
 
     public class doIt extends AsyncTask<Void, Void, Void>{
+
+        String words;
 
         @Override
         protected Void doInBackground(Void... voids) {
 
             try {
                 Document doc = (Document) Jsoup.connect("https://www.gonzaga.edu/").get();
-                
+                words = doc.getTextContent();
 
             }catch (Exception e){
                 e.printStackTrace();
             }
 
             return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+            texx.setText(words);
+
         }
     }
 
