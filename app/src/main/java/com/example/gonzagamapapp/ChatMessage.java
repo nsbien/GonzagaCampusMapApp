b2878c56a0a290b628799dc97e15a68e0c51b8d3
Joseph Torii <jtorii@zagmail.gonzaga.edu>
2019-12-06 18:33:46 -0800

    trying to connect firebase

---
+++ app/src/main/java/com/example/gonzagamapapp/ChatMessage.java
@@ -0,0 +1,37 @@
+package com.example.gonzagamapapp;
+
+public class ChatMessage {
+    private String author;
+    private String content;
+
+    public ChatMessage() {
+        author = "BLANK AUTHOR";
+        content = "BLANK CONTENT";
+    }
+
+    public ChatMessage(String author, String content) {
+        this.author = author;
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "From: " + author + "\n" + content;
+    }
+}
